ad9559175e4c6e8b712c2531f7002589cbc3b98c
soumyaurugonda <soumya.urugonda@gmail.com>
2023-06-11 14:13:41 +0000

    congrats_card

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  // Write your code here.
+  <div className="bg-container">
+    <div>
+      <h1>Congratulations</h1>
+    </div>
+    <div className="profile-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran V</h1>
+      <p>Vishnu Institute of Computer Education and Technology,Bhimavaram.</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
